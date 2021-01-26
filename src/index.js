3f1a62bde3fa3aba59681199c9a254f7b8c94332
erickes5trada <erick.e.senado@gmail.com>
2021-01-26 12:50:10 -0600

    se mueve carpets styles y modifica index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/js/bootstrap';
-import '../styles/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+import './styles/style.css';
 import './reloj.js';
 
 
